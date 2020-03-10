4cb3c9be217f51b17a968e8ed1240c3d4561cf96
vyacheslavv44 <follow.trend7777@gmail.com>
2020-03-11 00:29:09 +0400

    feat: add solution to task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+
+    let transformToString = n.toString();
+    let splitedN = transformToString.split('');
+    let reversedN = splitedN.reverse();
+    let joinedN = reversedN.join('');
+
+    return (parseFloat(joinedN));
+    
   
 }
